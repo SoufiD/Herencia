1fc7650e41c6533ead2b4bf20cb03cc23987d7db
Sofía Díaz <soufito21@gmail.com>
2020-12-10 11:34:17 -0500

    Insertar persona cambiando dinamicamente el archivo properties

--- src/main/java/com/clearminds/sdi/impl/PersonaManager.java
+++ src/main/java/com/clearminds/sdi/impl/PersonaManager.java
@@ -1,5 +1,10 @@
 package com.clearminds.sdi.impl;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.clearminds.sdi.excepciones.InstanceException;
 import com.clearminds.sdi.interfaces.ServicioPersona;
 import com.clearminds.sdi.model.Persona;
@@ -7,10 +12,16 @@ import com.clearminds.sdi.model.Persona;
 public class PersonaManager {
 	ServicioPersona serv;
 	
+	
+	
 	public PersonaManager() throws InstanceException{
 		Class<?> clase;
+		Properties p = new Properties();
+		
 		try {
-			clase = Class.forName("com.clearminds.sdi.impl.ServicioPersonaBDD");
+
+			p.load(new FileReader("clases.properties"));
+			clase = Class.forName(p.getProperty("clase"));
 			serv=(ServicioPersona) clase.newInstance();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -24,6 +35,14 @@ public class PersonaManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new InstanceException("Error al obtener el archivo");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new InstanceException("Error al obtener el archivo");
 		}
 
 	}
